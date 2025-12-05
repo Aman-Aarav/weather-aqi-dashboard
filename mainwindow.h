4790f5a061952874edf750ace2937911ec8f0a04
Aman Sharma <62217135+Aman-Aarav@users.noreply.github.com>
2025-12-05 12:05:14 +0530

    Updated slots

--- mainwindow.h
+++ mainwindow.h
@@ -3,18 +3,34 @@
 
 #include <QMainWindow>
 #include "weatherclient.h"
+#include <QPushButton>
 #include "QTimer"
 #include "QDateTime"
 #include "gauge.h"
 #include <QSlider>
 #include <QPropertyAnimation>
 #include "circleicons.h"
-
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QCategoryAxis>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QValueAxis>
+#include "forecastwidget.h"
+#include "QButtonGroup"
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+struct DailyTemp {
+    QString date;
+    double minTemp;
+    double maxTemp;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -27,9 +43,14 @@ public:
 private slots:
     void on_Searchbtn_clicked();
     void updateClock();
-    void on_dayNightBtn_toggled(bool checked);
+    //void on_dayNightBtn_toggled(bool checked);
     void onCurrentReceived(const WeatherData &data);
     void onAQIReceived(const AqiData &data);
+    void onForecastDataReceived(const QVector<ForecastEntry> &data);
+    void onDayButtonClicked();  // connected to all 5 day buttons
+    void clearLayout(QLayout *layout);
+    void setBtnEnabled();
+
 
 private:
     Ui::MainWindow *ui;
@@ -43,6 +64,20 @@ private:
     CircleIcons *maroon;
     CircleIcons *orange;
     CircleIcons *reddishorange;
+    void setIcon(const QString &path);
+    QChartView* createFeelsLikeChart(const QVector<ForecastEntry> &forecast);
+    QVector<DailyTemp> computeDailyMinMax(const QVector<ForecastEntry> &forecast);
+    QChartView* createMinMaxChart(const QVector<ForecastEntry> &forecast);
+    QChartView *TempchartView=nullptr;
+
+    QChartView *MinMaxchartView=nullptr;
+    QVector<ForecastWidget*> dayWidgets;
+    QVector<QPushButton*> dayButtons; // store your 5 buttons
+     QVector<ForecastEntry> forecastData;
+    int timezone;
+     QButtonGroup *buttonGroup;
+
+     bool isLoading = false;
 
 
     WeatherClient *WeatherObj;
